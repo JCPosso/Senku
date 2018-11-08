9f0761ec829d169b043e68693ada7bf8225c5c3e
JCPosso <44146884+JCPosso@users.noreply.github.com>
2018-11-07 23:38:29 -0500

    Update PanelTableroPrueba.java

--- PanelTableroPrueba.java
+++ PanelTableroPrueba.java
@@ -14,47 +14,65 @@ public class PanelTableroPrueba extends JPanel{
 	private final Dimension screenSize=Toolkit.getDefaultToolkit().getScreenSize();
 	private	int Ancho=screenSize.width/2;
 	private	int Alto=screenSize.height/2;
-	private Color colorFicha;
-	private ArrayList<Integer> posiciones;
+	private Color colorFicha;	
 	private final int FICHA_SIZE=10;
 	private final int BORDE_FICHA=8;
 	private int size ;
+	private int tamano;
+	private ArrayList<Integer> posiciones ;
 	private char[][] tablero;
 	private void setTablero(char[][] tableroNuevo){
 		tablero=tableroNuevo;
 		size = tablero.length;
+	}
+	private void crearTablero(int mini_col, int n_filas){
+		tablero = new char[n_filas][n_filas];
+		tamano=n_filas;
+		/*dibuja cruz del centro*/
+		int mid=(n_filas/2 + n_filas%2)-1;
+		int cont=0;
+		dibujarCruz(mid);
+		tablero[mid][mid]='x';
+		cont++;
+		/*dibuja el resto de las cruces*/
+		int avanzar=mid;
+		int retroceder=mid;
+		while( cont!=mini_col) {
+			if (cont%2==0){
+				avanzar++;
+				dibujarCruz(avanzar);
+			}
+			else{
+				retroceder--;
+				dibujarCruz(retroceder);
+			}
+			cont++;
+		}
+	}
+	private void dibujarCruz(int mid){
+		for (int iter = 0; iter < tamano; iter++) {
+			tablero[iter][mid]='+';
+			tablero[mid][iter]='+';
+		}
 	}
 	public PanelTableroPrueba(){
 		setBackground(Color.blue);
 		posiciones= new ArrayList<Integer>();
 		colorFicha=Color.red;
 		setPreferredSize(new Dimension(Ancho,Alto)); 
-		setTablero(new char[][] { 				{'o','o','o','+','+','+','o','o','o',},
-									{'o','o','o','+','+','+','o','o','o'},
-									{'o','o','o','+','+','+','o','o','o'},
-									
-									{'+','+','+','+','+','+','+','+','+'},
-									{'+','+','+','+','x','+','+','+','+'},
-									{'+','+','+','+','+','+','+','+','+'},
-									
-									{'o','o','o','+','+','+','o','o','o'},
-									{'o','o','o','+','+','+','o','o','o'},
-									{'o','o','o','+','+','+','o','o','o'},
-	}
-	);
+		crearTablero(1,5);
+		repaint();
 	}
 	 @Override
-public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         if (tablero != null){
             int posX = BORDE_FICHA;
             int posY = BORDE_FICHA;
-            for (int y = 0; y < size; y++) {
-                for (int x = 0; x < size; x++) {
-
+            for (int y = 0; y < tablero.length; y++) {
+                for (int x = 0; x < tablero.length; x++) {
                     if (tablero[y][x] == '+') {
-                        pintaCirculo(g, posX, posY,  colorFicha);
+                        pintaCirculo(g, posX, posY,  Color.red);
                     } else if (tablero[y][x] == 'x') {
                         pintaCirculo(g, posX, posY, Color.yellow);
                     } else if (tablero[y][x] == 'o') {
@@ -77,7 +95,7 @@ public void paintComponent(Graphics g) {
 		int espacioMaximoFicha = (FICHA_SIZE * 2) + BORDE_FICHA ;
         int posY = (ev.getX() - (BORDE_FICHA/ 2)) / espacioMaximoFicha;
         int posX = (ev.getY() - (BORDE_FICHA/ 2)) / espacioMaximoFicha;
-		
+		int tamano_Tablero =8;
 		if (posiciones.size()==2){
 			jugar((int)posiciones.get(0),(int)posiciones.get(1),posX,posY);
 			System.out.println(posiciones.get(0)+" "+posiciones.get(1)+" "+posX+" "+posY);
@@ -142,9 +160,6 @@ public void paintComponent(Graphics g) {
 	
 	public void fichasColor(){
 		colorFicha=JColorChooser.showDialog(null,"Escoge el color de las fichas.",colorFicha);
-		if (colorFicha==null){
-			colorFicha=Color.red;
-		}
 		repaint();
 	}
 }
